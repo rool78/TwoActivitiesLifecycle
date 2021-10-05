688eb597f9046153aae1bc7481b809f8d98045cb
rooL <raul.mazaa@gmail.com>
2021-10-05 11:11:19 +0200

    ..

---
+++ app/src/androidTest/java/com/example/myapplication/ActivityInputOutputTest.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.content.Context;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.android.twoactivities.MainActivity;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest {
+
+
+    /**
+     * Use {@link ActivityScenarioRule} to create and launch the activity under test, and close it
+     * after test completes. This is a replacement for {@link androidx.test.rule.ActivityTestRule}.
+     */
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.android.twoactivities", appContext.getPackageName());
+    }
+}
